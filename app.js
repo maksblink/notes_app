280a160d776d1c66617200148cb9755c656356d4
maks <blinkmaks777@gmail.com>
2022-08-29 12:35:33 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read note.',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all titles of notes.',
+    handler: function () {
+        notes.listNotes()
+    }
+})
+
+yargs.parse()
